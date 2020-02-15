9f2456d4562c9dc360e774a18c6aa174cbbfc284
Themi <Tcm4760@gmail.com>
2020-02-14 22:56:29 -0600

    General code organization:   -Changed triple single quotes to triple double quotes

--- loadfromdir.py
+++ loadfromdir.py
@@ -1,9 +1,9 @@
-'''
+"""
 An iterated prisoner's dilemma written by Arthur Goldman
 for Southwest High School in Minneapolis's Computer Science class 2016-2017
 arthur@goldman-tribe.org
 agol1801@mpsedu.org
-'''
+"""
 import glob
 
 
